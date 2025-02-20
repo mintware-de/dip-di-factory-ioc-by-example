171bba3882e4318e1800ef0e850ef42c3d6cec5b
Julian Finkler <julian@mintware.de>
2025-02-20 08:23:58 +0100

    change: Dependency injection

--- TripPlanner/TripPlanner.cs
+++ TripPlanner/TripPlanner.cs
@@ -6,11 +6,12 @@ public class TripPlanner
 {
     private readonly IRouteCalculator _routeCalculator;
 
-    private readonly Gps _gps = new Gps();
+    private readonly Gps _gps;
 
-    public TripPlanner()
+    public TripPlanner(IRouteCalculator routeCalculator, Gps gps)
     {
-        _routeCalculator = CalculatorFactory.CalculatorFactory.Create();
+        _routeCalculator = routeCalculator;
+        _gps = gps;
     }
 
     public void PlanTripFromCurrentLocation(string destination)
